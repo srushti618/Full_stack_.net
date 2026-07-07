104e0b2947e2e73de7e6db761b7ed075b93d11ef
srushti618 <deshmukhsrushti007@gmail.com>
2026-07-07 15:59:00 +0530

    2nd day data or files

---
+++ 7Jul/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+class Program {
+    static void Main() {
+        // ****variables and datatypes****
+//      long mobno=1663828632;
+//       int number=2345;
+//        double pi=3.14;
+//        bool active=true;
+//       Console.WriteLine(number);
+//       Console.WriteLine(mobno);
+//        Console.WriteLine(pi);
+//       Console.WriteLine(active);
+
+      //****Operators****
+       //  Console.WriteLine("Arithmetic operator");
+       // int a = 5;
+       // int b = 25;
+       // Console.WriteLine(a + b);
+       // Console.WriteLine(a - b);
+       // Console.WriteLine(a * b);
+       // Console.WriteLine(a / b);
+       // Console.WriteLine(a % b);
+
+       // Console.WriteLine("Assignment operator");
+       // a *= 5;
+       // b += 5;
+       // Console.WriteLine("a *= 5 → " + a);
+       // Console.WriteLine("b += 5 → " + b);
+
+       // Console.WriteLine("Comparison operator");
+       //  Console.WriteLine("a > b → " + (a > b));
+       // Console.WriteLine("a < b → " + (a < b));
+       // Console.WriteLine("a >= b → " + (a >= b));
+       // Console.WriteLine("a <= b → " + (a <= b));
+       // Console.WriteLine("a == b → " + (a == b));
+
+       // Console.WriteLine("Logical operator");
+       // Console.WriteLine("a > 0 && b > 0 → " + (a > 0 && b > 0));
+       // Console.WriteLine("a > 0 || b > 0 → " + (a > 0 || b > 0));
+
+       // Console.WriteLine("Increment and decrement");
+       // Console.WriteLine("a++ → " + (a++));
+       // Console.WriteLine("b++ → " + (b++));
+       // Console.WriteLine("--a → " + (--a));
+       // Console.WriteLine("--b → " + (--b));
+
+
+  //****Conditional Statements****
+//        int marks=85;
+
+//        if(marks>40 && marks<=50){
+//         Console.WriteLine("Your are Pass and You marks is "+(marks)+"your grade is C");
+//        }
+//        else if(marks>50 && marks<=70){
+// Console.WriteLine("Your are Pass and You marks is "+(marks)+"your grade is B");
+//        }
+//        else if(marks>70 && marks<=100){
+//         Console.WriteLine("Your are Pass and You marks is "+(marks)+"your grade is A");
+//        }
+//        else{
+//         Console.WriteLine("Your are Fail and You marks is "+(marks));
+//        }
+
+//****Loops*****
+//while loop
+//do-while loop
+//for loop
+//continue
+//break
+// An automated conveyor belt processes 20 packages. Package IDs are generated from 1001 to 1020 using a loop.
+// For each package:
+// If the package ID is divisible by 4, it is marked as Quality Check Required.
+// Else if the package ID is divisible by 5, it is marked as Priority Shipment.
+// Otherwise, it is marked as Normal Processing.
+// At the end of the program, display:
+// Total packages processed
+// Number of packages requiring quality check
+// Number of priority shipments
+// Number of normal packages
+
+
+// int totalpackagesprocess=0;
+
+// int prqc=0;
+
+// int priorityshipments=0;
+
+// int normalpackages=0;
+
+// for(int i=1001;i<=1020;i++){
+//        totalpackagesprocess ++;
+
+//        if(i%4==0){
+//               Console.WriteLine("This Package ID "+(i)+"  it is Required Quality Check Required.");
+//               prqc++;
+ 
+//        }
+//        else if(i%5==0){
+//               Console.WriteLine("This Package ID "+(i)+" it is Required  as Priority Shipment.");
+//               priorityshipments++;
+//        }
+//        else{
+// Console.WriteLine("This Package ID "+(i)+" it is Required normal packages. ");
+// normalpackages++;
+//        }
+// }
+// Console.WriteLine("The number of totalpackagesprocess is :  "+(totalpackagesprocess));
+// Console.WriteLine("The number of prqc is :  "+(prqc));
+// Console.WriteLine("The number of priorityshipments is :  "+(priorityshipments));
+// Console.WriteLine("The number of normalpackages is :  "+ (normalpackages));
+   
+// A smart city has 30 street lights numbered 1 to 30. The power consumption (in watts) for each light is calculated using the formula:
+// Power = 80 + (Light Number × 5)
+// For each street light:
+// If power consumption is greater than 180 W, display "Maintenance Required".
+// Else if power consumption is between 140 W and 180 W, display "Normal Operation".
+// Otherwise, display "Energy Efficient".
+// Also calculate and display:
+// Total power consumed by all street lights
+// Average power consumption
+// Number of lights in each category
+// int total_power=0;
+// int maintlight=0;
+// int normallight=0;
+// int energyelight=0;
+// int power =0;
+//  for(int i=1;i<=30;i++){
+// power =80+(i*5); 
+// total_power+=power;
+// if(power>180){
+//         Console.WriteLine("Maintenance Required.");
+//         maintlight++;
+// }
+// else if (power>140 && power<=180){
+//        Console.WriteLine("Normal operation");
+//        normallight++;
+// }
+// else{
+//        Console.WriteLine("Energy Efficient");
+//        energyelight++;
+// }
+//  }
+//  int avgpower=(total_power/30);
+
+// Console.WriteLine("The total power consumed by all street lights is : "+(total_power));
+// Console.WriteLine("The number of lights in each category is : "+(maintlight));
+// Console.WriteLine("The number of lights in each category is : "+(normallight));
+// Console.WriteLine("The number of lights in each category is : "+(energyelight));
+// Console.WriteLine("Theaverage power consumed by all street lights is : "+(avgpower));
+
+
+//        }
+       
+//     }
+
+
